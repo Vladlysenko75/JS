978f502c79a55963e964ee15c48d242ababb2f8f
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2021-12-13 03:50:25 +0200

    Classwork 12

--- additional_12/add_12.js
+++ additional_12/add_12.js
@@ -4,3 +4,28 @@
 //     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
 //     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
 
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(arr => {
+        for (let obj of arr) {
+            console.log(obj)
+
+            let box = document.createElement('div');
+            let boxId = document.createElement('div');
+            let boxBody = document.createElement('div');
+            let boxTitle = document.createElement('div');
+            let btn = document.createElement('button');
+
+            boxId.innerText = obj.id;
+            boxBody.innerText = obj.body;
+            btn.innerText = 'Show Title';
+
+            box.append(boxId,boxBody,btn);
+            document.body.appendChild(box);
+
+            btn.onclick = function () {
+                boxTitle.innerText = obj.title;
+                box.appendChild(boxTitle);
+            }
+        }
+    })
--- classwork_12/cw_12.js
+++ classwork_12/cw_12.js
@@ -6,24 +6,54 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(resp => resp.json())
     .then(arr => {
-        for (let obj of arr) {
-            let box = document.createElement('div');
-            let boxId = document.createElement('div');
-            let boxBody = document.createElement('div');
-            let boxTitle = document.createElement('div');
-            let btn = document.createElement('button');
+            for (let obj of arr) {
+                let box = document.createElement('div');
+                let boxId = document.createElement('div');
+                let boxBody = document.createElement('div');
+                let boxTitle = document.createElement('div');
+                let btn = document.createElement('button');
 
-            boxId.innerText = obj.id;
-            boxBody.innerText = obj.body;
-            btn.innerText = 'Show Title';
+                box.style.border = '5px dashed blue'
+                box.style.margin = '5px'
+                box.style.padding = '5px'
 
-            box.append(boxId,boxBody,btn);
-            document.body.appendChild(box);
+                boxId.innerText = `ID: ${obj.id}`;
+                boxBody.innerText = `Body: ${obj.body}`;
+                boxTitle.innerText = `Title: ${obj.title}`;
+                btn.innerText = 'Show Title';
 
-            btn.onclick = function () {
-                boxTitle.innerText = obj.title;
-                box.appendChild(boxTitle);
+                box.append(boxId, boxBody, boxTitle, btn);
+                document.body.appendChild(box);
+
+                btn.onclick = function () {
+                    fetch('https://jsonplaceholder.typicode.com/comments')
+                        .then(resp => resp.json())
+                        .then(coms => {
+                                for (let com of coms) {
+                                    if (obj.id === com.postId) {
+                                        let inBox = document.createElement('div');
+                                        let comBody = document.createElement('div');
+                                        let email = document.createElement('div');
+                                        let comID = document.createElement('div');
+                                        let name = document.createElement('div');
+
+                                        comBody.innerText = `Body: ${com.body}`;
+                                        email.innerText = `Email: ${com.email}`;
+                                        comID.innerText = `ID: ${com.id}`;
+                                        name.innerText = `Name: ${com.name}`;
+
+                                        inBox.style.border = '5px solid red';
+                                        inBox.style.margin = '5px';
+                                        inBox.style.padding = '5px';
+
+                                        inBox.append(comBody,email,comID,name);
+                                        box.appendChild(inBox)
+                                    }
+
+                            }
+                        })
+                }
+
             }
-
         }
-    })
+    )
