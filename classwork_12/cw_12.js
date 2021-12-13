0bac8e0fd086938de26a0fd0bf1cd654b612240f
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2021-12-13 03:12:30 +0200

    Classwork 12

---
+++ classwork_12/cw_12.js
@@ -0,0 +1,31 @@
+// 1.
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// https://jsonplaceholder.typicode.com/posts
+//     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+
+let loca = document.getElementById('here');
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(resp => resp.json())
+    .then(arr => {
+        for (let obj of arr) {
+            console.log(obj)
+            let box = document.createElement('div');
+            let boxId = document.createElement('div');
+            let boxBody = document.createElement('div');
+            let boxTitle = document.createElement('div');
+            let btn = document.createElement('button');
+
+            boxId.innerText = obj.id;
+            boxBody.innerText = obj.body;
+            btn.innerText = 'Show Title';
+
+            box.append(boxId,boxBody,btn);
+            document.body.appendChild(box);
+
+            btn.onclick = function () {
+                boxTitle.innerText = obj.title;
+                box.appendChild(boxTitle);
+            }
+
+        }
+    })
