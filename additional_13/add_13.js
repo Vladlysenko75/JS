0806481c2d950b81bf742ef4aca71e8018b1522e
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2021-12-14 20:58:57 +0200

    Additional 13

--- additional_13/add_13.js
+++ additional_13/add_13.js
@@ -13,12 +13,18 @@ let products = 'products';
 
 let saveProduct = (productName, productQuantity, productPrice, productImg) => {
     let newArr = JSON.parse(localStorage.getItem(products)) || [];
-    newArr.push({productName, productQuantity, productPrice, productImg})
+    if (newArr.length > 0) {
+        let sortArr = newArr.sort((a, b) => a.id - b.id);
+        let id = sortArr[sortArr.length - 1].id + 1;
+        newArr.push({ id: id, productName, productQuantity, productPrice, productImg})
+    } else {
+        newArr.push({id: 1, productName, productQuantity, productPrice, productImg});
+    }
     localStorage.setItem(products, JSON.stringify(newArr));
 };
 document.forms.f1.onsubmit = function (e) {
     e.preventDefault()
-    saveProduct(productName.value,productQuantity.value,productPrice.value,productImg.value)
+    saveProduct(productName.value, productQuantity.value, productPrice.value, productImg.value)
 }
 // створити елемент <a href='list.html'> На сторінку товарів</a>, та list.html, при переході на який відобразити на сторінці всі товари.
 // На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
--- additional_13/list.js
+++ additional_13/list.js
@@ -6,23 +6,37 @@ for (let i = 0; i < cart.length; i++) {
     let productImg = document.createElement('img');
     let productBox = document.createElement('div');
     let productBtn = document.createElement('button');
+    let deleteAll = document.createElement('button');
 
     productBtn.innerText = 'Delete this product';
+    deleteAll.innerText = 'Delete All posts';
 
     productName.innerText = cart[i].productName;
     productQuantity.innerText = cart[i].productQuantity;
     productPrice.innerText = cart[i].productPrice;
     productImg.src = cart[i].productImg;
 
-    productBox.append(productName,productQuantity,productPrice,productImg,productBtn);
-    document.body.appendChild(productBox);
+    productBox.append(productName, productQuantity, productPrice, productImg, productBtn);
+    document.body.append(productBox, deleteAll);
 
     productBtn.onclick = function () {
-        if (cart[i]) {
-            window.location.reload();
-            cart.splice(i, 1);
-            localStorage.setItem('products', JSON.stringify(cart));
-            cart[i].remove();
+
+        window.location.reload();
+        cart = cart.filter(item => item.id !== cart[i].id);
+        localStorage.setItem('products', JSON.stringify(cart));
+        cart[i].remove();
+    }
+
+    deleteAll.onclick = function () {
+        window.location.reload();
+        cart.splice(0,cart.length);
+        localStorage.setItem('products', JSON.stringify(cart));
+        for (let prods of cart) {
+            prods.remove()
         }
     }
 }
+let arrs = [1, 3, 4, 5]
+console.log(arrs)
+arrs.splice(0, arrs.length);
+console.log(arrs)
